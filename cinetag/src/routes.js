9a3c8452843891ce78664e3c38935a80f02922e8
stfnrosa <rosaa.estefanii@gmail.com>
2024-06-18 19:10:54 -0300

    criando contexto e um novo hook

--- cinetag/src/routes.js
+++ cinetag/src/routes.js
@@ -1,12 +1,24 @@
+import Cabecalho from "components/Cabecalho"
+import Container from "components/Container"
+import Rodape from "components/Rodape"
+import FavoritosProvider from "contextos/favoritos"
+import Favoritos from "pages/Favoritos"
 import Inicio from "pages/Inicio"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function AppRoutes() {
-    return(
+    return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio/>}></Route>
-            </Routes>
+            <Cabecalho />
+            <Container>
+                <FavoritosProvider>
+                    <Routes>
+                        <Route path="/" element={<Inicio />}></Route>
+                        <Route path="/favoritos" element={<Favoritos />}></Route>
+                    </Routes>
+                </FavoritosProvider>
+            </Container>
+            <Rodape />
         </BrowserRouter>
     )
 }
